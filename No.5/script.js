d30a6aacf950cda21a2b3860895f2b2722ca8fd6
vjviolet <gxzj_2015@163.com>
2017-03-08 20:05:28 +0800

    the fifth

---
+++ No.5/script.js
@@ -0,0 +1,10 @@
+function toGreen() {
+    document.getElementById("div1").style.backgroundColor = "green";
+    document.getElementById("div1").style.height = "100px";
+    document.getElementById("div1").style.width = "200px";
+}
+
+function toRed() {
+    document.getElementById("div1").style.backgroundColor = "red";
+    document.getElementById("div1").style.width = "100px";
+}
