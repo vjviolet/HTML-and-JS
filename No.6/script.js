9008d58476daa2db0a15ab92d608671a205367b6
vjviolet <gxzj_2015@163.com>
2017-03-08 20:05:59 +0800

    the sixth

---
+++ No.6/script.js
@@ -0,0 +1,22 @@
+// function click1() {
+//     var link1 = document.getElementById("link");
+//     link1.href = "style1.css"
+// }
+
+// function click2() {
+//     var link2 = document.getElementById("link");
+//     link2.href = "style2.css";
+// }
+
+// function changeText() {
+//     document.getElementById("text1").value("xiaozhou");
+// }
+
+function show() {
+    var div = document.getElementById("div");
+    if(div.style.display=="none") {
+        div.style.display = "block";
+    } else {
+        div.style.display = "none";
+    }
+}
